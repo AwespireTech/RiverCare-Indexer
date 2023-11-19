f5b7ce642a8e130fb484dcc895a0a158acf25051
timothy <r10922188@csie.ntu.edu.tw>
2023-11-20 03:29:49 +0800

    wip: loop ready

--- main.go
+++ main.go
@@ -16,5 +16,6 @@ func main() {
 	for _, river := range res {
 		tezos.PrintContractStorage(river)
 	}
+	tezos.Monitor()
 
 }
--- tezos/monitor.go
+++ tezos/monitor.go
@@ -1,10 +1,12 @@
 package tezos
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 
+	"blockwatch.cc/tzgo/rpc"
 	"github.com/AwespireTech/InterfaceForCare-Backend/models"
 	"github.com/AwespireTech/InterfaceForCare-Indexer/config"
 	"github.com/AwespireTech/InterfaceForCare-Indexer/database"
@@ -41,6 +43,25 @@ func FullUpdate() error {
 		}
 	}
 	return nil
+}
+func Monitor() {
+	client := GetClient()
+	mon := rpc.NewBlockHeaderMonitor()
+	defer mon.Close()
+	client.MonitorBlockHeader(context.Background(), mon)
+	for {
+		block, err := mon.Recv(context.Background())
+		if err != nil {
+			log.Panicln(err)
+		}
+		fmt.Println(block.Hash)
+
+		// err = FullUpdate()
+		// if err != nil {
+		// 	log.Panicln(err)
+		// }
+	}
+
 }
 func UpdateRiver(river models.River) error {
 	//Parse EventData to Id
